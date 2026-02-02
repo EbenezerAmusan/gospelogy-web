31622856ec0adcdbc72951fa42debb685d7065cf
Ebenezer Amusan <114451891+EbenezerAmusan@users.noreply.github.com>
2026-02-02 13:08:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   base: "/",
 });
 
+export default defineConfig({
+  base: "/",
+});
+
+
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
